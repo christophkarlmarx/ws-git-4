3216900c235cbbeb9866ea506f06dea5cf814f59
Christoph Marx <christoph.marx@de.bosch.com>
2023-10-26 09:24:03 +0200

    my first commit cmx

--- Bosch.WebApi/ApiConfiguration.cs
+++ Bosch.WebApi/ApiConfiguration.cs
@@ -7,6 +7,7 @@
             Console.WriteLine("Hallo Welt");
             Console.WriteLine("123");
             Console.WriteLine("jetzt aber...");
+            Console.WriteLine("my first commit...");
         }
 
         public void SayHello(){
